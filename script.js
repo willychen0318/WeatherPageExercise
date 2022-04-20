cabe1bb093efb4eaa2dc90c3fd080e916189251b
willychen0318 <willy03180201@gmail.com>
2022-04-20 18:02:45 +0800

    js api display result and change html text

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 let weather ={
     "apiKey":"b01f116f6bb757cca7ff8ec9d20f2f6b",
     fetchWeather: function(city){
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid="+ this.apiKey)
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid="+ this.apiKey)
         .then((response)=>response.json())
-        .then((data)=>console.log(data));
+        .then((data)=>this.displayWeather(data));
     },
-    
+    displayWeather: function(data){
+        const{ name }= data;
+        const{ icon, description }= data.weather[0];
+        const{ temp, humidity }= data.main;
+        const{ speed }=data.wind;
+        console.log(name,icon,description,temp,humidity,speed)
+        document.querySelector(".city").innerText="Weather in "+name;
+        document.querySelector(".temp").innerText=temp+"°C";
+        document.querySelector(".icon").src="https://openweathermap.org/img/wn/"+icon+"@2x.png";
+        document.querySelector(".description").innerText=description;
+        document.querySelector(".humidity").innerText="Humidity: "+humidity+"%";
+        document.querySelector(".wind").innerText="Wind Speed: "+speed+" km/h";
+    },
 
 };
